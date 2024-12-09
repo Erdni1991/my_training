9938b9d5a9e5caaf51e4101cfe4cc4b0bf1c840f
Erdni1991 <erdnya08@gmail.com>
2024-12-09 18:59:04 +0300

    Add files via upload

---
+++ переменные.py
@@ -0,0 +1,4 @@
+number_of_tasks_complited=12
+number_of_hours_spent=1.5
+course_title='python'
+print(course_title,number_of_tasks_complited,number_of_hours_spent,number_of_hours_spent/number_of_tasks_complited)
